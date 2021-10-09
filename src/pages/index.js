3784862e87e63ee6c04bd18641d313a0a61b8402
Ajay <ajayr4217@gmail.com>
2021-10-09 13:16:55 +0530

    fix error

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from "react"
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
+    <main>
       <title>Home Page</title>
       <h1>Hi, I'm Ajay</h1>
     </main>
